071248862f53ecd55252facd32f899412b2773d2
Afonso ComboVideos <afonso.bezerra@combovideos.com.br>
2022-01-12 22:30:00 -0300

    Parte Empresa

--- Blumo-html/assets/script/inicio.js
+++ Blumo-html/assets/script/inicio.js
@@ -2,13 +2,11 @@ const btn =  document.getElementById('btn');
 const main = document.getElementById('main');
 btn.addEventListener('click' , () =>{
      btn.classList.toggle('active');
-     main.classList.toggle('zindex');
-
-})
+ })
 
 $('.owl-carousel').owlCarousel({
     center: true,
-    items:2,
+    items:1,
     loop:true,
     margin:10,
     zindex:99999,
