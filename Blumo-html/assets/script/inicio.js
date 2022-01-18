7c25e3104c83b3a8e74f62391d77b524d852dd1e
Afonso ComboVideos <afonso.bezerra@combovideos.com.br>
2022-01-17 21:52:39 -0300

    Designer Mobile
    Quase Concluído

--- Blumo-html/assets/script/inicio.js
+++ Blumo-html/assets/script/inicio.js
@@ -5,10 +5,47 @@ btn.addEventListener('click' , () =>{
  })
 
 $('.owl-carousel').owlCarousel({
-    center: true,
     items:1,
     loop:true,
     margin:10,
     zindex:99999,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed:850
+
 });
+
+$('.owl-carousel-depoimento').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    zindex:99999,
+});
+
+
+
+const button = document.getElementById('button-retangulo');
+
+
+const buttonredondo = document.getElementById('whatsapp-fixed');
+
+
+document.addEventListener('scroll' , () => {
+
+
+   const posibutton =  button.getBoundingClientRect();
+
+   if(posibutton.y <= 837 && posibutton.y > 70 ){
+       buttonredondo.style.opacity='0'
+       button.style.opacity = '1'
+   }else{
+       buttonredondo.style.opacity ="1"
+       button.style.opacity = '0'
+   }
+})
+
+
+AOS.init();
 
