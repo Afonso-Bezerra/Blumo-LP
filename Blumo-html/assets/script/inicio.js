85c17ea6633fbf2b1cb380640fd94ba70090c9d1
Afonso ComboVideos <afonso.bezerra@combovideos.com.br>
2022-01-18 23:43:52 -0300

    Mobile - Finalizado

--- Blumo-html/assets/script/inicio.js
+++ Blumo-html/assets/script/inicio.js
@@ -4,26 +4,56 @@ btn.addEventListener('click' , () =>{
      btn.classList.toggle('active');
  })
 
-$('.owl-carousel').owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    zindex:99999,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    smartSpeed:850
+const conteudo =  document.getElementById('conteudo');
 
-});
+ conteudo.addEventListener('click', () =>{
+    if(document.getElementById('menucheck').checked == true){
+        document.getElementById('menucheck').checked = false
+        btn.classList.toggle('active');
+    }
+ })
 
-$('.owl-carousel-depoimento').owlCarousel({
-    center: true,
-    items:2,
-    loop:true,
-    margin:10,
-    zindex:99999,
-});
 
+ $(() => {
+    $('#cassossel1').owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        zindex:99999,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        smartSpeed:850,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                stagePadding: 10,
+            },
+            321:{
+                stagePadding: 50,
+            }
+        }
+    });
+    $('#cassossel2').owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        zindex:999,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        smartSpeed:850,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                stagePadding: 70,
+            },
+            321:{
+                stagePadding: 80,
+            }
+        }
+    });
+  });
 
 
 const button = document.getElementById('button-retangulo');
@@ -49,3 +79,34 @@ document.addEventListener('scroll' , () => {
 
 AOS.init();
 
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    bgcolor: 'transparent', 
+    maxWidth: '80%',
+    spinner: 'swing',
+});
+
+// const linksFake = document.querySelectorAll('#caixa');
+// const descCaixaContainer = document.getElementById('descContainer')
+// linksFake.forEach((linkFake) => {
+//   linkFake.addEventListener('click', (ev) => {
+//     const currentElemet = ev.currentTarget.getAttribute("data-caixa");
+//     const caixaSelector = currentElemet.split(' ')[1]
+//     const currentDesc = document.getElementById(currentElemet)
+//     descCaixaContainer.style.display = "flex";
+//     descCaixaContainer.style.opacity= "1";
+//     currentDesc.style.display = "flex";
+//     document.body.style.overflow = 'hidden'
+//     descCaixaContainer.addEventListener('click' , () => {
+//           descCaixaContainer.style.opacity= "0";
+//         setTimeout(() =>{
+//             descCaixaContainer.style.display = "none";
+//             currentDesc.style.display = "none";
+//         },500)
+//         document.body.style.overflow = 'auto'
+//     })
+//   })
+// })
